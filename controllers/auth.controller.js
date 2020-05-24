f07165df825ed85cc77a0645d8d07b4057c88d57
Quang Tu Tran <tutq8118@gmail.com>
2020-05-24 15:56:11 +0700

    update-security

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const db = require('../db');
 const md5 = require('md5');
+const bcrypt  = require('bcrypt');
 
 module.exports = {
   login: (req, res) => {
@@ -9,8 +10,8 @@ module.exports = {
     var email = req.body.email;
     var password = req.body.password;
     var hashedPassword = md5(password);
-    console.log(md5('123123'));
     var user = db.get('users').find({email: email}).value();
+    
     if (!user) {
       res.render('auth/login', {
         errors: [
@@ -19,14 +20,39 @@ module.exports = {
       });
       return;
     }
-    if (hashedPassword !== user.password) {
+    if (user.wrongLoginCount > 2) {
       res.render('auth/login', {
         errors: [
-          "Wrong password"
+          "Your account is temporarily locked because your password was wrong over 4 times. Please contact us to unlock"
         ]
       });
       return;
     }
+
+    if (!bcrypt.compareSync(password, user.password)) {     
+      user.wrongLoginCount ++;
+      res.render('auth/login', {
+        errors: [
+          `Wrong password! You have ${4 - user.wrongLoginCount} ${user.wrongLoginCount> 2? 'time' : 'times'} to try`
+        ]
+      });
+      return;
+    }
+
+    user.wrongLoginCount = 0;
+    
+    // Async 
+    // bcrypt.compare(password, user.password, function(err, result) {
+    //   if (!result) {
+    //     res.render('auth/login', {
+    //       errors: [
+    //         "Wrong password"
+    //       ]
+    //     });
+    //     return;
+    //   }
+    // });
+
     res.cookie('userID', user.id);
     res.redirect('/users');
   }
