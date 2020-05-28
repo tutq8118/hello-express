9c456a139f66aff11b6794e915d92d686552f000
Tu Tran <tutq3@fo.com>
2020-05-28 17:01:11 +0700

    Update: Upload image

--- middlewares/cookie.middleware.js
+++ middlewares/cookie.middleware.js
@@ -8,10 +8,11 @@ module.exports = {
     next();
   },
   check: (req, res, next) => {
+    const avartaUrl = db.get('users').find({id: req.signedCookies.userID}).value().avatarUrl;
     res.locals.curentUserEmail = req.signedCookies.userID !== null && req.signedCookies.userID !== undefined ? db.get('users').find({id: req.signedCookies.userID}).value().email : '';
     res.locals.isAdmin = req.signedCookies.userID !== null && req.signedCookies.userID !== undefined ? db.get('users').find({id: req.signedCookies.userID}).value().isAdmin : false;
     res.locals.userName = req.signedCookies.userID !== null && req.signedCookies.userID !== undefined? db.get('users').find({id: req.signedCookies.userID}).value().name: null;
-    res.locals.avatarUrl = req.signedCookies.userID !== null && req.signedCookies.userID !== undefined? db.get('users').find({id: req.signedCookies.userID}).value().avatarUrl: null;
+    res.locals.avatarUrl = avartaUrl !== null && avartaUrl !== undefined && avartaUrl !== '' ? avartaUrl : 'https://cdn.glitch.com/0ded82f9-2581-4657-8e03-011ea567f797%2Fdefault-avatar.png';
     next();
   }
 }
