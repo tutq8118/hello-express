fc1f7fe2737ebdf04d55c5e5792e27c32e6a64c9
Tu Tran <tutq3@fo.com>
2020-07-07 16:46:51 +0700

    Update: set header

--- server.js
+++ server.js
@@ -47,6 +47,13 @@ app.use(express.static('public'));
 app.use(cookieMiddleware.check);
 app.use(sesstionMiddleware.check);
 
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use('/users', authMiddleware.requireAuth, usersRoute);
 app.use('/books', booksRoute);
 app.use('/transactions', authMiddleware.requireAuth, transactionsRoute);
